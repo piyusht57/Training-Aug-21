7e19300423786067850fbf0a3801e039cb14dda3
piyusht57 <piyushthakur572000@gmail.com>
2021-09-15 17:19:02 +0530

    REFS #926 fixes issue #926 worked with various json functions

--- SQL/Piyushsingh Thakur/DAY-10/JSON.sql
+++ SQL/Piyushsingh Thakur/DAY-10/JSON.sql
@@ -1,3 +1,18 @@
+USE DAY14
+--1) Create a batch Select Banking as ‘Bank Dept’, Insurance as ‘Insurance Dept’ and Services as ‘Services Dept’ from 
+--   employee table
+SELECT *,
+	CASE 
+		WHEN Department='Banking' THEN 'Bank Department'
+		WHEN Department='Insurance' THEN 'Insurance Department'
+		WHEN Department='Services' THEN 'Services Department'
+		ELSE Department
+	END AS Department
+	FROM Employee
+GO
+
+
+
 CREATE TABLE STUDENT
     (NAME VARCHAR(200),
     ADDRESS NVARCHAR(MAX),
@@ -8,11 +23,11 @@ CREATE TABLE STUDENT
 
  
 
----- 5 Students Name, Address, City, DOB, CLASS need to be inserted in the student table, need to fetch these result from json variable. 
----- and select output in the json format.
+--2) 5 Students Name, Address, City, DOB, CLASS need to be inserted in the student table, need to fetch these result from json variable. 
+--   and select output in the json format.
 DECLARE @STUDENT NVARCHAR(MAX)
 SELECT @STUDENT =   
-  N'[  
+  '[  
        {  
          "Name": "JAY",
 		 "Address": "KHODIYAR NAGAR",
