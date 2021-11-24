0e6ad9eaf2a262216bc6ae4738e6c2d01a56f27d
piyusht57 <piyushthakur572000@gmail.com>
2021-11-24 14:21:48 +0530

    REFS:#2096 and fixes #2096 Worked on event ,lambda and delegates

---
+++ C#/Piyushsingh Thakur/Day-6/Assignment/Assignment/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+day-6
+Compute area of rectangle using func delegate
+
+Compute add of two number using lambda expression 
+*/
+
+
+
+
+using System;
+
+namespace Assignment
+{
+    class Program
+    {
+        public delegate Area Func<in x, in y, out Area>(int x, int y);
+
+        public static int AreaMethod(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            // Compute area of rectangle using func delegate
+            int a, b;
+
+            Console.Write("Enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter width : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Func<int, int, int> area = AreaMethod;
+            Console.WriteLine("Area of rectangle : " + area(a, b));
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine( );
+
+            // Compute add of two number using lambda expression
+            Func<int, int, int> Sum = (p, q) => p + q;
+            Console.WriteLine("Addition : " + Sum(a, b));
+
+        }
+    }
+}
